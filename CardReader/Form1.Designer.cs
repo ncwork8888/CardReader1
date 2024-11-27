bfcb5db3b3e16514c28ad797e4e3e0fefa23751a
ncwork8888 <ncwork8888@gmail.com>
2024-11-27 18:01:53 +0800

    WFSOpen, WFSRegister, WFSIsBlocking, WFSGetInfo succeed

--- CardReader/Form1.Designer.cs
+++ CardReader/Form1.Designer.cs
@@ -28,43 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.WFSStartUP = new System.Windows.Forms.Button();
-            this.WFSOpen = new System.Windows.Forms.Button();
-            this.WFSRegister = new System.Windows.Forms.Button();
+            this.WFSStartUPBtn = new System.Windows.Forms.Button();
+            this.WFSOpenBtn = new System.Windows.Forms.Button();
+            this.WFSRegisterBtn = new System.Windows.Forms.Button();
             this.WFSStartUpResult = new System.Windows.Forms.TextBox();
             this.WFSOpenResult = new System.Windows.Forms.TextBox();
             this.WFSRegisterResult = new System.Windows.Forms.TextBox();
+            this.WFSLockBtn = new System.Windows.Forms.Button();
+            this.WFSLockResult = new System.Windows.Forms.TextBox();
+            this.WFSGetInfoBtn = new System.Windows.Forms.Button();
+            this.WFSGetInfoResult = new System.Windows.Forms.TextBox();
+            this.WFSIsBlockingBtn = new System.Windows.Forms.Button();
+            this.WFSIsBlockingResult = new System.Windows.Forms.TextBox();
+            this.WFSAsyncExecuteBtn = new System.Windows.Forms.Button();
+            this.WFSAsyncExecuteResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // WFSStartUp
+            // WFSStartUPBtn
             // 
-            this.WFSStartUP.Location = new System.Drawing.Point(55,75);
-            this.WFSStartUP.Name = "WFSStartUp";
-            this.WFSStartUP.Size = new System.Drawing.Size(147, 25);
-            this.WFSStartUP.TabIndex = 0;
-            this.WFSStartUP.Text = "WFSStartUp";
-            this.WFSStartUP.UseVisualStyleBackColor = true;
-            this.WFSStartUP.Click += new System.EventHandler(this.WFSStartUp_Click);
+            this.WFSStartUPBtn.Location = new System.Drawing.Point(55, 75);
+            this.WFSStartUPBtn.Name = "WFSStartUPBtn";
+            this.WFSStartUPBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSStartUPBtn.TabIndex = 0;
+            this.WFSStartUPBtn.Text = "WFSStartUp";
+            this.WFSStartUPBtn.UseVisualStyleBackColor = true;
+            this.WFSStartUPBtn.Click += new System.EventHandler(this.WFSStartUp_Click);
             // 
-            // WFSOpen
+            // WFSOpenBtn
             // 
-            this.WFSOpen.Location = new System.Drawing.Point(55, 116);
-            this.WFSOpen.Name = "WFSOpen";
-            this.WFSOpen.Size = new System.Drawing.Size(147, 25);
-            this.WFSOpen.TabIndex = 1;
-            this.WFSOpen.Text = "WFSOpen";
-            this.WFSOpen.UseVisualStyleBackColor = true;
-            this.WFSOpen.Click += new System.EventHandler(this.WFSOpen_Click);
+            this.WFSOpenBtn.Location = new System.Drawing.Point(55, 116);
+            this.WFSOpenBtn.Name = "WFSOpenBtn";
+            this.WFSOpenBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSOpenBtn.TabIndex = 1;
+            this.WFSOpenBtn.Text = "WFSOpen";
+            this.WFSOpenBtn.UseVisualStyleBackColor = true;
+            this.WFSOpenBtn.Click += new System.EventHandler(this.WFSOpen_Click);
             // 
-            // WFSRegister
+            // WFSRegisterBtn
             // 
-            this.WFSRegister.Location = new System.Drawing.Point(55, 153);
-            this.WFSRegister.Name = "WFSRegister";
-            this.WFSRegister.Size = new System.Drawing.Size(147, 25);
-            this.WFSRegister.TabIndex = 2;
-            this.WFSRegister.Text = "WFSRegister";
-            this.WFSRegister.UseVisualStyleBackColor = true;
-            this.WFSRegister.Click += new System.EventHandler(this.WFSRegister_Click);
+            this.WFSRegisterBtn.Location = new System.Drawing.Point(55, 153);
+            this.WFSRegisterBtn.Name = "WFSRegisterBtn";
+            this.WFSRegisterBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSRegisterBtn.TabIndex = 2;
+            this.WFSRegisterBtn.Text = "WFSRegister";
+            this.WFSRegisterBtn.UseVisualStyleBackColor = true;
+            this.WFSRegisterBtn.Click += new System.EventHandler(this.WFSRegister_Click);
             // 
             // WFSStartUpResult
             // 
@@ -87,17 +95,93 @@
             this.WFSRegisterResult.Size = new System.Drawing.Size(411, 22);
             this.WFSRegisterResult.TabIndex = 5;
             // 
+            // WFSLockBtn
+            // 
+            this.WFSLockBtn.Location = new System.Drawing.Point(55, 193);
+            this.WFSLockBtn.Name = "WFSLockBtn";
+            this.WFSLockBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSLockBtn.TabIndex = 6;
+            this.WFSLockBtn.Text = "WFSLock";
+            this.WFSLockBtn.UseVisualStyleBackColor = true;
+            this.WFSLockBtn.Click += new System.EventHandler(this.WFSLockBtn_Click);
+            // 
+            // WFSLockResult
+            // 
+            this.WFSLockResult.Location = new System.Drawing.Point(208, 194);
+            this.WFSLockResult.Name = "WFSLockResult";
+            this.WFSLockResult.Size = new System.Drawing.Size(411, 22);
+            this.WFSLockResult.TabIndex = 7;
+            // 
+            // WFSGetInfoBtn
+            // 
+            this.WFSGetInfoBtn.Location = new System.Drawing.Point(55, 233);
+            this.WFSGetInfoBtn.Name = "WFSGetInfoBtn";
+            this.WFSGetInfoBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSGetInfoBtn.TabIndex = 8;
+            this.WFSGetInfoBtn.Text = "WFSGetInfo";
+            this.WFSGetInfoBtn.UseVisualStyleBackColor = true;
+            this.WFSGetInfoBtn.Click += new System.EventHandler(this.WFSGetInfoBtn_Click);
+            // 
+            // WFSGetInfoResult
+            // 
+            this.WFSGetInfoResult.Location = new System.Drawing.Point(208, 236);
+            this.WFSGetInfoResult.Name = "WFSGetInfoResult";
+            this.WFSGetInfoResult.Size = new System.Drawing.Size(411, 22);
+            this.WFSGetInfoResult.TabIndex = 9;
+            // 
+            // WFSIsBlockingBtn
+            // 
+            this.WFSIsBlockingBtn.Location = new System.Drawing.Point(55, 264);
+            this.WFSIsBlockingBtn.Name = "WFSIsBlockingBtn";
+            this.WFSIsBlockingBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSIsBlockingBtn.TabIndex = 10;
+            this.WFSIsBlockingBtn.Text = "WFSIsBlocking";
+            this.WFSIsBlockingBtn.UseVisualStyleBackColor = true;
+            this.WFSIsBlockingBtn.Click += new System.EventHandler(this.WFSIsBlockingBtn_Click);
+            // 
+            // WFSIsBlockingResult
+            // 
+            this.WFSIsBlockingResult.Location = new System.Drawing.Point(208, 267);
+            this.WFSIsBlockingResult.Name = "WFSIsBlockingResult";
+            this.WFSIsBlockingResult.Size = new System.Drawing.Size(411, 22);
+            this.WFSIsBlockingResult.TabIndex = 11;
+            // 
+            // WFSAsyncExecuteBtn
+            // 
+            this.WFSAsyncExecuteBtn.Location = new System.Drawing.Point(55, 295);
+            this.WFSAsyncExecuteBtn.Name = "WFSAsyncExecuteBtn";
+            this.WFSAsyncExecuteBtn.Size = new System.Drawing.Size(147, 25);
+            this.WFSAsyncExecuteBtn.TabIndex = 12;
+            this.WFSAsyncExecuteBtn.Text = "WFSAsyncExecute";
+            this.WFSAsyncExecuteBtn.UseVisualStyleBackColor = true;
+            this.WFSAsyncExecuteBtn.Click += new System.EventHandler(this.WFSAsyncExecuteBtn_Click);
+            // 
+            // WFSAsyncExecuteResult
+            // 
+            this.WFSAsyncExecuteResult.Location = new System.Drawing.Point(208, 298);
+            this.WFSAsyncExecuteResult.Name = "WFSAsyncExecuteResult";
+            this.WFSAsyncExecuteResult.Size = new System.Drawing.Size(411, 22);
+            this.WFSAsyncExecuteResult.TabIndex = 13;
+            // 
             // Form1
             // 
-            this.Controls.Add(this.WFSStartUP);
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(631, 251);
+            this.ClientSize = new System.Drawing.Size(676, 457);
+            this.Controls.Add(this.WFSAsyncExecuteResult);
+            this.Controls.Add(this.WFSAsyncExecuteBtn);
+            this.Controls.Add(this.WFSIsBlockingResult);
+            this.Controls.Add(this.WFSIsBlockingBtn);
+            this.Controls.Add(this.WFSGetInfoResult);
+            this.Controls.Add(this.WFSGetInfoBtn);
+            this.Controls.Add(this.WFSLockResult);
+            this.Controls.Add(this.WFSLockBtn);
+            this.Controls.Add(this.WFSStartUPBtn);
             this.Controls.Add(this.WFSRegisterResult);
             this.Controls.Add(this.WFSOpenResult);
             this.Controls.Add(this.WFSStartUpResult);
-            this.Controls.Add(this.WFSRegister);
-            this.Controls.Add(this.WFSOpen);
+            this.Controls.Add(this.WFSRegisterBtn);
+            this.Controls.Add(this.WFSOpenBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,13 +190,20 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button WFSStartUP;
-        private System.Windows.Forms.Button WFSOpen;
-        private System.Windows.Forms.Button WFSRegister;
+        private System.Windows.Forms.Button WFSStartUPBtn;
+        private System.Windows.Forms.Button WFSOpenBtn;
+        private System.Windows.Forms.Button WFSRegisterBtn;
         private System.Windows.Forms.TextBox WFSStartUpResult;
         private System.Windows.Forms.TextBox WFSOpenResult;
         private System.Windows.Forms.TextBox WFSRegisterResult;
-        
+        private System.Windows.Forms.Button WFSLockBtn;
+        private System.Windows.Forms.TextBox WFSLockResult;
+        private System.Windows.Forms.Button WFSGetInfoBtn;
+        private System.Windows.Forms.TextBox WFSGetInfoResult;
+        private System.Windows.Forms.Button WFSIsBlockingBtn;
+        private System.Windows.Forms.TextBox WFSIsBlockingResult;
+        private System.Windows.Forms.Button WFSAsyncExecuteBtn;
+        private System.Windows.Forms.TextBox WFSAsyncExecuteResult;
     }
 }
 
--- CardReader/Form1.cs
+++ CardReader/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 namespace CardReader
 {
@@ -20,17 +21,24 @@ namespace CardReader
             public string SzDescription { get => szDescription; set => szDescription = value; }
         }
 
-        private const int WFS_ERR_CANCELED = -1;
-        private const int WFS_ERR_CONNECTION_LOST = -2;
-        private const int WFS_ERR_INTERNAL_ERROR = -15;
+        // Event classes
+        private const uint SERVICE_EVENTS = 0x00000001;
+        private const uint USER_EVENTS = 0x00000002;
+        private const uint SYSTEM_EVENTS = 0x00000004;
+        private const uint EXECUTE_EVENTS = 0x00000008;
+
+        //
+        private const uint WFS_IDC_TRACK1 = 0x00000001;
+        private const uint WFS_IDC_TRACK2 = 0x00000002;
+        private const uint WFS_IDC_TRACK3 = 0x00000004;
 
         // Importing the necessary functions from the DLLs
         [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         public static extern int WFSStartUp(uint dwVersionsRequired, out WFSVersion lpWFSVersion);
 
 
-        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", EntryPoint = "WFSOpen", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-        public static extern int Native_WFSOpen(
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern int WFSOpen(
             string lpszLogicalName,
             uint hApp,
             string lpszAppID,
@@ -40,16 +48,82 @@ namespace CardReader
             ref WFSVersion lpSrvcVersion,
             ref WFSVersion lpSPIVersion,
             ref IntPtr lphService
-            );
+        );
 
         [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-        public static extern int XFSRegister(int hService, IntPtr pRegister);
+        public static extern int WFSRegister(IntPtr hService, uint dwEventClass, IntPtr hWndReg);
 
-        
+
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int WFSCreateAppHandle(out IntPtr lphApp);
+
+
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern int WFSLock(IntPtr hService, uint dwTimeOut, out IntPtr lppResult);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WFSRESULT
+        {
+            public IntPtr hService;       // Service handle
+            public uint RequestID;        // Request ID
+            public uint hResult;          // Result code
+            public IntPtr lpBuffer;       // Pointer to the result buffer
+            public uint uTimeout;         // Timeout duration
+        }
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern int WFSFreeResult(IntPtr lpResult);
+
+
         private IntPtr hService;
+
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern int WFSGetInfo(
+            IntPtr hService,
+            uint dwCategory,
+            IntPtr lpQueryDetails,
+            uint dwTimeOut,
+            out IntPtr lppResult
+        );
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool WFSIsBlocking();
+
+        [DllImport(@"C:\Windows\SysWOW64\msxfs.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern int WFSAsyncExecute(
+            IntPtr hService,            // Handle to the service provider
+            uint dwCommand,             // Command to execute
+            uint lpCmdData,           // Data structure for the command
+            uint dwTimeOut,             // Timeout in milliseconds
+            IntPtr hWnd             // Window handle for completion message
+            //out uint lpRequestID        // Pointer to request ID for the operation
+        );
+
+
+
 
         public Form1()
         {
+            try
+            {
+
+                WFSVersion versionInfo = new WFSVersion(); // Initialize the structure
+
+
+                uint versionRequired = 0x00030003;
+
+                int hResult = WFSStartUp(versionRequired, out versionInfo);
+                if (hResult != 0)
+                {
+
+                    return; // Exit if startup fails
+                }
+            }
+            catch { }
+
             InitializeComponent();
         }
 
@@ -57,9 +131,9 @@ namespace CardReader
         {
             try
             {
-                
+
                 WFSVersion versionInfo = new WFSVersion(); // Initialize the structure
-                uint versionRequired = 3; 
+                uint versionRequired = 0x00030003;
 
                 int hResult = WFSStartUp(versionRequired, out versionInfo);
                 if (hResult != 0)
@@ -86,22 +160,32 @@ namespace CardReader
 
         private void WFSOpen_Click(object sender, EventArgs e)
         {
-            WFSOpenResult.Text = "Ads";
+
             try
             {
-                string logicalName = "IDC30"; // Replace with your device logical name
-                uint hApp = 0; // Application handle (can be 0 if not needed)
+                IntPtr appHandle = IntPtr.Zero;
+                int result = WFSCreateAppHandle(out appHandle);
+
+                if (result != 0)
+                {
+                    WFSOpenResult.Text = $"WFSCreateAppHandle failed with error: {result}";
+                    return;
+                }
+
+                string logicalName = "IDC30"; // Replace with your device's logical name
+                uint hApp = (uint)appHandle.ToInt32(); // Correct handle conversion
                 string appID = ""; // Application identifier
-                uint traceLevel = 0; // Trace level (1 for example)
+                uint traceLevel = 0x0000000A; // Trace level (adjust as needed)
                 uint timeout = 18000; // Timeout in milliseconds
                 uint versionsRequired = 0x00030003; // Service version required
 
                 WFSVersion srvcVersion = new WFSVersion();
                 WFSVersion spiVersion = new WFSVersion();
 
-                hService = IntPtr.Zero; // Initialize the service handle
+                hService = IntPtr.Zero; // Initialize service handle
 
-                int hResult = Native_WFSOpen(
+                // Call WFSOpen instead of Native_WFSOpen
+                int hResult = WFSOpen(
                     logicalName,
                     hApp,
                     appID,
@@ -115,16 +199,7 @@ namespace CardReader
 
                 if (hResult != 0)
                 {
-                    string v = hResult switch
-                    {
-                        WFS_ERR_CANCELED => "The request was canceled by WFSCancelBlockingCall.",
-                        WFS_ERR_CONNECTION_LOST => "The connection to the service is lost.",
-                        WFS_ERR_INTERNAL_ERROR => "An internal inconsistency or unexpected error occurred.",
-                        _ => $"Unknown error occurred. HRESULT: {hResult}"
-                    };
-                    string errorMessage = v;
-
-                    WFSOpenResult.Text = $"WFSOpen failed: {errorMessage}";
+                    WFSOpenResult.Text = $"WFSOpen failed with error code: {hResult}";
                     return;
                 }
 
@@ -142,8 +217,184 @@ namespace CardReader
 
         private void WFSRegister_Click(object sender, EventArgs e)
         {
-            WFSRegisterResult.Text = "Ads";
+            if (hService == IntPtr.Zero)
+            {
+                WFSRegisterResult.Text = "Invalid service handle. Ensure WFSOpen succeeded.";
+                return;
+            }
 
+            uint eventClasses = SERVICE_EVENTS| SYSTEM_EVENTS | USER_EVENTS | EXECUTE_EVENTS;
+            IntPtr hWndReg = this.Handle;
+
+            int hResult = WFSRegister(hService, eventClasses, hWndReg);
+
+            if (hResult != 0)
+            {
+                WFSRegisterResult.Text = $"WFSRegister failed with error code: {hResult}";
+            }
+            else
+            {
+                WFSRegisterResult.Text = "WFSRegister succeeded. Event monitoring enabled.";
+            }
+        }
+
+        private void WFSLockBtn_Click(object sender, EventArgs e)
+        {
+            if (hService == IntPtr.Zero)
+            {
+                WFSLockResult.Text = "Service handle is invalid. Ensure WFSOpen was successful.";
+                return;
+            }
+
+            try
+            {
+                IntPtr pResult;
+                uint timeout = 30000; // Timeout in milliseconds (adjust as needed)
+
+                int hResult = WFSLock(hService, timeout, out pResult);
+
+                if (hResult == 0) // WFS_SUCCESS
+                {
+                    WFSRESULT result = Marshal.PtrToStructure<WFSRESULT>(pResult);
+                    WFSLockResult.Text = $"WFSLock succeeded. Request ID: {result.RequestID}";
+
+                    // Free the result memory
+                    WFSFreeResult(pResult);
+                }
+                else
+                {
+                    WFSLockResult.Text = $"WFSLock failed with error code: {hResult}";
+                }
+            }
+            catch (EntryPointNotFoundException ex)
+            {
+                WFSLockResult.Text = $"Entry point not found: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                WFSLockResult.Text = $"Unexpected error: {ex.Message}";
+            }
+        }
+
+        private void WFSGetInfoBtn_Click(object sender, EventArgs e)
+        {
+            if (hService == IntPtr.Zero)
+            {
+                WFSGetInfoResult.Text = "Invalid service handle.";
+                return;
+            }
+
+            try
+            {
+                
+                uint dwCategory = 0xC9; 
+
+                IntPtr lpQueryDetails = IntPtr.Zero; // No extra query details needed
+                uint timeout = 30000; // Timeout in milliseconds
+                IntPtr pResult;
+
+                int hResult = WFSGetInfo(hService, dwCategory, lpQueryDetails, timeout, out pResult);
+
+                if (hResult == 0) // WFS_SUCCESS
+                {
+                    WFSRESULT result = Marshal.PtrToStructure<WFSRESULT>(pResult);
+                    WFSGetInfoResult.Text = $"WFSGetInfo succeeded. Request ID: {result.RequestID}";
+
+                    // Free the result memory
+                    WFSFreeResult(pResult);
+                }
+                else
+                {
+                    WFSGetInfoResult.Text = $"WFSGetInfo failed with error code: {hResult}";
+                }
+            }
+            catch (Exception ex)
+            {
+                WFSGetInfoResult.Text = $"Unexpected error: {ex.Message}";
+            }
+        }
+
+        private void WFSIsBlockingBtn_Click(object sender, EventArgs e)
+        {
+            if (hService == IntPtr.Zero)
+            {
+                WFSIsBlockingResult.Text = "Invalid service handle.";
+                return;
+            }
+
+            // Check if a blocking operation is in progress
+            if (WFSIsBlocking())
+            {
+                WFSIsBlockingResult.Text = "Another blocking operation is in progress.";
+                return;
+            }
+
+            try
+            {
+                uint dwCategory = 0xC9; // WFS_INF_IDC_STATUS (IDC status information)
+                IntPtr lpQueryDetails = IntPtr.Zero; // No additional query details required for status
+                uint timeout = 30000; // Timeout in milliseconds (30 seconds)
+                IntPtr pResult;
+
+                // Call WFSGetInfo to get the IDC status
+                int hResult = WFSGetInfo(hService, dwCategory, lpQueryDetails, timeout, out pResult);
+
+                if (hResult == 0) // WFS_SUCCESS
+                {
+                    WFSRESULT result = Marshal.PtrToStructure<WFSRESULT>(pResult);
+                    WFSIsBlockingResult.Text = $"WFSGetInfo succeeded. Request ID: {result.RequestID}";
+
+                    // Free the result memory
+                    WFSFreeResult(pResult);
+                }
+                else
+                {
+                    WFSIsBlockingResult.Text = $"WFSGetInfo failed with error code: {hResult}";
+                }
+            }
+            catch (Exception ex)
+            {
+                WFSIsBlockingResult.Text = $"Unexpected error: {ex.Message}";
+            }
+        }
+
+        private void WFSAsyncExecuteBtn_Click(object sender, EventArgs e)
+        {
+            if (hService == IntPtr.Zero)
+            {
+                WFSAsyncExecuteResult.Text = "Invalid service handle.";
+                return;
+            }
+
+            // Check if a blocking operation is in progress
+            if (WFSIsBlocking())
+            {
+                WFSAsyncExecuteResult.Text = "Another blocking operation is in progress.";
+                return;
+            }
+
+            try
+            {
+                uint dwCommand = 0xCF; // Replace with an actual command for your device
+                uint lpCmdData = WFS_IDC_TRACK1 | WFS_IDC_TRACK2 | WFS_IDC_TRACK3;  
+                uint dwTimeOut = 30000; // Timeout (in milliseconds)
+
+                // Call WFSAsyncExecute to send the command to the service provider
+                int hResult = WFSAsyncExecute(hService, dwCommand, lpCmdData, dwTimeOut, this.Handle);
+
+                if (hResult == 0) // WFS_SUCCESS
+                {
+                    WFSAsyncExecuteResult.Text = $"WFSAsyncExecute succeeded. Request ID: ";
+                }
+                else
+                {
+                    WFSAsyncExecuteResult.Text = $"WFSAsyncExecute failed with error: {hResult}";
+                }
+            }
+            catch (Exception ex)
+            {
+                WFSAsyncExecuteResult.Text = $"Error during WFSAsyncExecute: {ex.Message}";
+            }
         }
     }
 }
